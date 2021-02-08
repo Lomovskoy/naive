a548da62a26e5579eb76dff00ead217f5cffaa3d
lomovskoy <lomovskoy.kirill@yandex.ru>
2021-02-08 21:15:35 +0300

    #1 Создал рабочий каркас приложения

---
+++ src/main/java/com/naive/server/naive/NaiveApplication.java
@@ -0,0 +1,13 @@
+package com.naive.server.naive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naive/server/naive/NaiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naive.server.naive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
